be8402d15bb83fc0514708048a4df90d9aaf2217
Tobitic <tibotic@gmail.com>
2016-08-10 12:16:20 +0700

    Remove build

--- templates/pokemongo-bot/1/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#set -e
-
-echo "Run  username=${POKE_USERNAME}"
-echo "Run  location=${POKE_LOCATION}"
-exec python pokecli.py -u "$POKE_USERNAME" -p "$POKE_PASSWORD" -l "$POKE_LOCATION"
